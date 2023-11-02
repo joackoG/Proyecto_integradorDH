0ec80a01703a5fdde61552a647ff04e6694f0fea
kurupi01 <joacko234.jg@gmail.com>
2023-11-02 12:25:47 -0300

    ediciones de footer y direcciones

--- app.js
+++ app.js
@@ -17,3 +17,12 @@ app.listen(port, ()=>{
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname,'views','login.html'))
    })
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','register.html'))
+   })
+app.get('/productCart', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','productCart.html'))
+   })
+   app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','productDetail.html'))
+   })
