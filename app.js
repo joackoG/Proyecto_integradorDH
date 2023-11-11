be190240d0d4203f4b00eb368b99af7fcc5ed3bf
kurupi01 <joacko234.jg@gmail.com>
2023-11-10 22:48:47 -0300

    ediciones del index

--- app.js
+++ app.js
@@ -5,31 +5,24 @@ const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'))
 })
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`El servidor esta funcionando en el puerto ${port}
     http://localhost:${port}`);
 })
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','login.html'))
-   })
+    res.sendFile(path.join(__dirname, 'views', 'login.html'))
+})
 app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','register.html'))
-   })
+    res.sendFile(path.join(__dirname, 'views', 'register.html'))
+})
 app.get('/productCart', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','productCart.html'))
-   })
-   app.get('/productDetail', (req, res) => {
-    res.sendFile(path.join(__dirname,'views','productDetail.html'))
-   })
-   document.getElementById('siguiente').addEventListener('click', function() {
-    document.querySelector('ofertas').scrollLeft += 300; // Ajusta el valor según el ancho de cada elemento
-  });
-  
-  document.getElementById('anterior').addEventListener('click', function() {
-    document.querySelector('ofertas').scrollLeft -= 300; // Ajusta el valor según el ancho de cada elemento
-  });
+    res.sendFile(path.join(__dirname, 'views', 'productCart.html'))
+})
+app.get('/productDetail', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'productDetail.html'))
+})
