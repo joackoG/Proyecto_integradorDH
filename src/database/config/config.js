3277dedd398fef975c76c1777a293e3ee45f7c7a
kurupi01 <joacko234.jg@gmail.com>
2024-03-02 22:15:54 -0300

    ignorar

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -3,7 +3,7 @@ module.exports=
 {
   "development": {
     "username": "root",
-    "password": "jazmin",
+    "password": "142314",
     "database": "shenlongcomics",
     "host": "127.0.0.1",
     "dialect": "mysql"
